493680255f4e930fabba2454a6626800a647a89f
ztarikul <ztarikulislam@gmail.com>
2025-05-24 17:58:28 +0600

    Sidebar polishing

--- backend/mjlforce/resources/views/components/master.blade.php
+++ backend/mjlforce/resources/views/components/master.blade.php
@@ -1,45 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="description"
-      content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities."
-    />
-    <meta
-      name="keywords"
-      content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app"
-    />
+    <meta name="description"
+        content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities." />
+    <meta name="keywords"
+        content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app" />
     <meta name="author" content="pixelstrap" />
     <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon" />
-    <link
-      rel="shortcut icon"
-      href="../assets/images/favicon.png"
-      type="image/x-icon"
-    />
+    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon" />
     <title>MJL FORCE - @yield('title')</title>
     <!-- Google font-->
     <link rel="preconnect" href="https://fonts.gstatic.com/" />
     <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet" />
     <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+        rel="stylesheet" />
     <link
-      href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet" />
     <!-- Font Awesome-->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="../assets/css/fontawesome.css"
-    />
+    <link rel="stylesheet" type="text/css" href="../assets/css/fontawesome.css" />
     <!-- ico-font-->
     <link rel="stylesheet" type="text/css" href="../assets/css/icofont.css" />
     <!-- Themify icon-->
@@ -47,71 +33,51 @@
     <!-- Flag icon-->
     <link rel="stylesheet" type="text/css" href="../assets/css/flag-icon.css" />
     <!-- Feather icon-->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="../assets/css/feather-icon.css"
-    />
+    <link rel="stylesheet" type="text/css" href="../assets/css/feather-icon.css" />
     <!-- Plugins css start-->
     <link rel="stylesheet" type="text/css" href="../assets/css/animate.css" />
     <link rel="stylesheet" type="text/css" href="../assets/css/chartist.css" />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="../assets/css/date-picker.css"
-    />
+    <link rel="stylesheet" type="text/css" href="../assets/css/date-picker.css" />
     <link rel="stylesheet" type="text/css" href="../assets/css/prism.css" />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="../assets/css/vector-map.css"
-    />
+    <link rel="stylesheet" type="text/css" href="../assets/css/vector-map.css" />
     <!-- Plugins css Ends-->
     <!-- Bootstrap css-->
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css" />
     <!-- App css-->
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
-    <link
-      id="color"
-      rel="stylesheet"
-      href="../assets/css/color-1.css"
-      media="screen"
-    />
+    <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen" />
     <!-- Responsive css-->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="../assets/css/responsive.css"
-    />
-  </head>
-  <body>
+    <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css" />
+</head>
+
+<body>
     <!-- Loader starts-->
     <div class="loader-wrapper">
-      <div class="theme-loader">
-        <div class="loader-p"></div>
-      </div>
+        <div class="theme-loader">
+            <div class="loader-p"></div>
+        </div>
     </div>
     <!-- Loader ends-->
     <!-- page-wrapper Start       -->
     <div class="page-wrapper compact-wrapper" id="pageWrapper">
-      <!-- Page Header Start-->
-      @include('components.header')
-      <!-- Page Header Ends                              -->
-      <!-- Page Body Start-->
-      <div class="page-body-wrapper sidebar-icon">
-        <!-- Page Sidebar Start-->
-        @include('components.sidebar')
-        <!-- Page Sidebar Ends-->
-        <div class="page-body">
-          <!-- Container-fluid starts-->
+        <!-- Page Header Start-->
+        @include('components.header')
+        <!-- Page Header Ends                              -->
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper sidebar-icon">
+            <!-- Page Sidebar Start-->
+            @include('components.sidebar')
+            <!-- Page Sidebar Ends-->
+            <div class="page-body">
+                <!-- Container-fluid starts-->
 
-          @yield('content')
-          
-          <!-- Container-fluid Ends-->
+                @yield('content')
+
+                <!-- Container-fluid Ends-->
+            </div>
+            <!-- footer start-->
+            @include('components.footer')
         </div>
-        <!-- footer start-->
-        @include('components.footer')
-      </div>
     </div>
     <!-- latest jquery-->
     <script src="../assets/js/jquery-3.5.1.min.js"></script>
@@ -157,7 +123,8 @@
     <script src="../assets/js/theme-customizer/customizer.js"></script>
     <!-- login js-->
     <!-- Plugin used-->
-  </body>
+</body>
 
-  <!-- Mirrored from admin.pixelstrap.com/viho/theme/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 26 Sep 2021 04:08:15 GMT -->
+<!-- Mirrored from admin.pixelstrap.com/viho/theme/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 26 Sep 2021 04:08:15 GMT -->
+
 </html>
