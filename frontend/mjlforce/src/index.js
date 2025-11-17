5ef091348dd8a726f4ac802e5cd3b77d211103cf
ztarikul <ztarikulislam@gmail.com>
2025-11-17 10:49:59 +0600

    assets to the public folder for css working

--- frontend/mjlforce/src/index.js
+++ frontend/mjlforce/src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
